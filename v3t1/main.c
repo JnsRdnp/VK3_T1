f30fef4fcd5f4fc0ac34a3c6a488621f04660d0c
JnsRdnp <joonasroska123@gmail.com>
2022-09-15 17:00:29 +0300

    Valmis

---
+++ v3t1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int whileCount = 0;
+
+//    while(whileCount<5){
+//        printf("100\n");
+//        whileCount +=1;
+//    }
+    while(whileCount<5){
+        printf("100 ");
+        whileCount +=1;
+    }
+
+    return 0;
+}
